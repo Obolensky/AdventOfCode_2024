43c07a79a07cff46293781b66b6c5b89ed622f92
Blue_Subaru <obolensky@outlook.fr>
2024-12-23 16:55:13 +0100

    Update main.cpp

--- Day2_Part2/main.cpp
+++ Day2_Part2/main.cpp
@@ -54,6 +54,18 @@ auto main() -> int {
         if (mainLoop(fileLine)) {
             safe++;
         }
+        else {
+            // A tester si ca decalle bien quand on insert dans le vector
+            for (int i = 0; i < fileLine.size(); i++) {
+                int temp = fileLine[i];
+                fileLine.erase(fileLine.begin() + i);
+                if (mainLoop(fileLine)) {
+                    safe++;
+                    break;
+                }
+                fileLine.insert(fileLine.begin() + i, temp);
+            }
+        }
     }
     inputFile.close();
     std::cout << safe << std::endl;
