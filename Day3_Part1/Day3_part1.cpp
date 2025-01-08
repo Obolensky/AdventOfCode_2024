3d17c48a261d5bb12feb3ff619c09dcd8e81af74
Blue_Subaru <obolensky@outlook.fr>
2025-01-08 15:10:46 +0100

    Update Day3_part1.cpp
    Working

--- Day3_Part1/Day3_part1.cpp
+++ Day3_Part1/Day3_part1.cpp
@@ -4,7 +4,7 @@
 #include <fstream>
 #include <iterator>
 
-std::string readToString(std::string filename) {
+auto readToString(const std::string& filename) -> std::string  {
     std::ifstream file(filename, std::ios_base::binary | std::ios_base::in);
     using Iterator = std::istreambuf_iterator<char>;
     std::string content(Iterator{file}, Iterator{});
@@ -15,22 +15,22 @@ auto main() -> int {
     std::cout << "--- Advent of code 2024 : Day 3 Part 1 ---" << std::endl;
 
     std::string contentString = readToString("input.txt");
-    int muli = 0;
     int total = 0;
-    std::string delimiter = ",";
-
-    std::regex regex1("[0-9]{1,3},[0-9]{1,3}");
+    std::regex regex1("mul\\([0-9]{1,3},[0-9]{1,3}\\)");
     std::smatch res;
     while (regex_search(contentString, res, regex1, std::regex_constants::match_any)) {
         std::string s = res[0];
-        std::string token1 = s.substr(0, s.find(delimiter));
-        std::string token2 = s.substr(1, s.find(delimiter));
-        token2 = token2.substr(0, token2.size()-1);
-        std::cout << token2 << std::endl;
-        /**
-        muli = stoi(token1) * stoi(token2);
-        total += muli;
-        **/
+        
+        // get 1st number
+        unsigned first = s.find('(');
+        unsigned last = s.find_last_of(',');
+        const int num1 = stoi(s.substr(first+1,last-first-1));
+        // get 2nd number
+        first = s.find(',');
+        last = s.find_last_of(')');
+        const int num2 = stoi(s.substr(first+1,last-first-1));
+        
+        total += num1*num2;
         contentString = res.suffix().str();
     }
     std::cout << total << std::endl;
